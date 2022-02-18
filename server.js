7f99874796e7b8343541f604de2e1db1be6db7af
Liam Ok <liamokane19@gmail.com>
2022-02-18 22:38:14 +1100

    adding json file to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const termData = require('./db/db.json');
+
 const app = express();
 const PORT = 3001;
 
@@ -14,6 +16,13 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+app.get('*', (req, res) =>
+    res.send(
+    `Make a GET request using Insomnia to <a href="http://localhost:${PORT}/api/terms">http://localhost:${PORT}/api/terms</a>`
+    )
+);
+
+app.get('/api/terms', (req, res) => res.json(termData));
 
 
 app.listen(PORT, () =>
