af52fb2978101210bffdc60dacc7183ed90a6532
Liam Ok <liamokane19@gmail.com>
2022-02-21 10:00:52 +1100

    applied a response object to send back to the client

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+
+// Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -43,7 +45,24 @@ app.post('/api/notes', (req, res) => {
     
     // Log our request to the terminal
     console.info(`${req.method} request received`);
-    });
+      // Prepare a response object to send back to the client
+        let response;
+
+        // Check if there is anything in the response body
+        if (req.body && req.body.product) {
+            response = {
+            status: 'success',
+            data: req.body,
+            };
+            res.json(`Review for ${response.data.product} has been added!`);
+        } else {
+            res.json('Request body must at least contain a product name');
+        }
+
+        // Log the response body to the console
+        console.log(req.body);
+
+});
 
 
 app.listen(PORT, () =>
