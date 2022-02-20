7dbba49e55f8b958f7fab61f57c3e22b93b711ab
Liam Ok <liamokane19@gmail.com>
2022-02-21 09:57:01 +1100

    adding middlewares so our data is converting data that we're sending our http post

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/index', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
