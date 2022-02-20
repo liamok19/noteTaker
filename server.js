89d997fc66c5366309409ce83e6e09d6930fa96b
Liam Ok <liamokane19@gmail.com>
2022-02-21 09:34:26 +1100

    updating server.js to respond ith a request method that the users post request has been received

--- server.js
+++ server.js
@@ -32,6 +32,16 @@ app.get('/api/db', (req, res) => res.json(dbData));
 
 app.get('/api/db:db', (req, res) => res.json(dbData));
 
+app.post('/api/reviews', (req, res) => {
+    // Let the client know that their POST request was received
+    res.json(`${req.method} request received`);
+    
+    // Show the user agent information in the terminal
+    console.info(req.rawHeaders);
+    
+    // Log our request to the terminal
+    console.info(`${req.method} request received`);
+    });
 
 
 app.listen(PORT, () =>
