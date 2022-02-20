c095e7d36f10193be8013caabc0ba8d3d1eb2e50
Liam Ok <liamokane19@gmail.com>
2022-02-21 10:53:32 +1100

    noticed the termData function isn't required cleaning up js and removed this. Also need to reserach routes. The current server.js is getting bigger then anticipated.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
-
 const termData = require('./db/db.json');
 
+//create an epxress server
 const app = express();
+//create a port number for the server to run off of 
 const PORT = 3001;
-
-app.use(express.static('public'));
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//holding CSS, JS and HTML files in a static middleware function in express.
+app.use(express.static('public'));
 
 app.get('/index', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -32,19 +34,17 @@ app.get('*', (req, res) => {
 
 });
 
-app.get('/api/db', (req, res) => res.json(dbData));
-
-app.get('/api/db:db', (req, res) => res.json(dbData));
 
 app.post('/api/notes', (req, res) => {
     // Let the client know that their POST request was received
-    res.json(`${req.method} request received`);
-    
+    // res.json(`${req.method} request received`);
+
     // Show the user agent information in the terminal
     console.info(req.rawHeaders);
     
-    // Log our request to the terminal
-    console.info(`${req.method} request received`);
+    // // Log our request to the terminal
+    // console.info(`${req.method} request received`);
+
       // Prepare a response object to send back to the client
         let response;
 
