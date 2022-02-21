51755861056acd369fd2d9b1140e099a065e00d9
Liam Ok <liamokane19@gmail.com>
2022-02-21 11:01:05 +1100

    updated server.js it now looks for an external file route for the api/notes 'get' and 'post' requests.

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ app.get('/index', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+
 
 app.get('*', (req, res) => {
     res.send(
@@ -33,35 +31,7 @@ app.get('*', (req, res) => {
 
 });
 
-
-app.post('/api/notes', (req, res) => {
-    // Let the client know that their POST request was received
-    // res.json(`${req.method} request received`);
-
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
-    
-    // // Log our request to the terminal
-    // console.info(`${req.method} request received`);
-
-      // Prepare a response object to send back to the client
-        let response;
-
-        // Check if there is anything in the response body
-        if (req.body && req.body.product) {
-            response = {
-            status: 'success',
-            data: req.body,
-            };
-            res.json(`Review for ${response.data.product} has been added!`);
-        } else {
-            res.json('Request body must at least contain a product name');
-        }
-
-        // Log the response body to the console
-        console.log(req.body);
-
-});
+require('./routes/notesRoutes')(app);
 
 
 app.listen(PORT, () =>
