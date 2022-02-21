151161627fcd7713c5a878db5a773a8e95861b0e
Liam Ok <liamokane19@gmail.com>
2022-02-21 11:40:57 +1100

    reverting from route files

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,38 +1,50 @@
 const fs = require ('fs');
 
-module.export = (app) => {
-    
-    app.get('/notes', (req, res) =>
-        res.sendFile(path.join(__dirname, 'public/notes.html'))
-    );
+const notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
 
-    app.post('/api/notes', (req, res) => {
-        // Let the client know that their POST request was received
-        // res.json(`${req.method} request received`);
+module.exports = function (app) {
+    
+    //APP get request. Reading the data from db.json file.
+    app.get('/api/notes', (req, res) => {
+    
+        res.sendFile(path.join(__dirname, 'public/notes.html'))
 
         // Show the user agent information in the terminal
         console.info(req.rawHeaders);
-        
-        // // Log our request to the terminal
-        // console.info(`${req.method} request received`);
 
         // Prepare a response object to send back to the client
-        let response;
+        let notesDB = notesData;
 
-        // Check if there is anything in the response body
-        if (req.body && req.body.product) {
-            response = {
-            status: 'success',
-            data: req.body,
-            };
-            res.json(`Review for ${response.data.product} has been added!`);
-        } else {
-            res.json('Request body must at least contain a product name');
-        }
+        console.log('\r\n GET request - Returning notes data: ' + JSON.stringify(notesDB));
 
-        // Log the response body to the console
-        console.log(req.body);
+        res.JSON = notesDB
+        
+    });
 
-});
+    // app.post('/api/notes', (req, res) => {
+    //     // Let the client know that their POST request was received
+    //     res.json(`${req.method} request received`);
 
-}
+    //     // Show the user agent information in the terminal
+    //     console.info(req.rawHeaders);
+        
+    //     // // Log our request to the terminal
+    //     // console.info(`${req.method} request received`);
+
+    //     // Check if there is anything in the response body
+    //     if (req.body && req.body.product) {
+    //         response = {
+    //         status: 'success',
+    //         data: req.body,
+    //         };
+    //         res.json(`Review for ${response.data.product} has been added!`);
+    //     } else {
+    //         res.json('Request body must at least contain a product name');
+    //     }
+
+    //     // Log the response body to the console
+    //     console.log(req.body);
+
+// });
+
+};
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const termData = require('./db/db.json');
+
 //create an epxress server
 const app = express();
 //create a port number for the server to run off of 
@@ -17,7 +19,9 @@ app.get('/index', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.get('*', (req, res) => {
     res.send(
@@ -31,8 +35,39 @@ app.get('*', (req, res) => {
 
 });
 
-require('./routes/notesRoutes')(app);
 
+app.post('/api/notes', (req, res) => {
+    // Let the client know that their POST request was received
+    // res.json(`${req.method} request received`);
+
+    // Show the user agent information in the terminal
+    console.info(req.rawHeaders);
+    
+    // // Log our request to the terminal
+    // console.info(`${req.method} request received`);
+
+      // Prepare a response object to send back to the client
+        let response;
+
+        // Check if there is anything in the response body
+        if (req.body && req.body.product) {
+            response = {
+            status: 'success',
+            data: req.body,
+            };
+            res.json(`Review for ${response.data.product} has been added!`);
+        } else {
+            res.json('Request body must at least contain a product name');
+        }
+
+        // Log the response body to the console
+        console.log(req.body);
+
+});
+
+app.get('/api/db', (req, res) => res.json(termData));
+
+app.get('/api/db:db', (req, res) => res.json(termData));
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
