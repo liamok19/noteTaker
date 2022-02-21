c9fb21b3b4f08c2e1f9605d01d151cc4a4301d7c
Liam Ok <liamokane19@gmail.com>
2022-02-21 13:59:18 +1100

    updating the server.js just focusing on app.get

--- server.js
+++ server.js
@@ -34,34 +34,34 @@ app.get('*', (req, res) => {
 });
 
 
-app.post('/api/notes', (req, res) => {
-    // Let the client know that their POST request was received
-    // res.json(`${req.method} request received`);
+// app.post('/api/notes', (req, res) => {
+//     // Let the client know that their POST request was received
+//     // res.json(`${req.method} request received`);
 
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
+//     // Show the user agent information in the terminal
+//     console.info(req.rawHeaders);
     
-    // // Log our request to the terminal
-    // console.info(`${req.method} request received`);
+//     // // Log our request to the terminal
+//     // console.info(`${req.method} request received`);
 
-      // Prepare a response object to send back to the client
-        let response;
+//       // Prepare a response object to send back to the client
+//         let response;
 
-        // Check if there is anything in the response body
-        if (req.body && req.body.product) {
-            response = {
-            status: 'success',
-            data: req.body,
-            };
-            res.json(`Review for ${response.data.product} has been added!`);
-        } else {
-            res.json('Request body must at least contain a product name');
-        }
+//         // Check if there is anything in the response body
+//         if (req.body && req.body.product) {
+//             response = {
+//             status: 'success',
+//             data: req.body,
+//             };
+//             res.json(`Review for ${response.data.product} has been added!`);
+//         } else {
+//             res.json('Request body must at least contain a product name');
+//         }
 
-        // Log the response body to the console
-        console.log(req.body);
+//         // Log the response body to the console
+//         console.log(req.body);
 
-});
+// });
 
 
 app.listen(PORT, () =>
